f7d1977e95c246cc9f4ffacb8dc051eb3124ba71
Iam_fortunatus <amfortunatus@gmail.com>
2023-01-10 00:55:23 +0100

    0x0A-python-inheritance

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
+"""
+Defines "Rectangle" class subclass of "Basegeometry"
+"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-"""
-===================================
-module with class Rectangle
-===================================
-"""
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class that inherits from BaseGeometry"""
-
+    """class Rectangle"""
     def __init__(self, width, height):
+        """Initializes a Rectangle"""
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
+"""
+Defines "Rectangle" class subclass of "Basegeometry"
+"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-"""
-===================================
-module with class BaseGeometry
-===================================
-"""
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class that inherits from BaseGeometry"""
-
+    """class Rectangle"""
     def __init__(self, width, height):
-        """Method for initialized the attrubutes"""
+        """Initializes a Rectangle"""
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
 
     def area(self):
-        """Method to redefine a area method in the parent class"""
-
+        """returns the area of the rectangle"""
         return self.__width * self.__height
 
     def __str__(self):
-        """__str__ method for return the next string"""
-
+        """returns the rectangle description"""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)

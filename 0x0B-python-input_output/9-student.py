8712169d66e78e2d829acb8dfaeaa118466e9643
Iam_fortunatus <amfortunatus@gmail.com>
2023-01-10 23:49:12 +0100

    0x0B-python-input_output

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-Student = __import__('9-student').Student
+"""
+Defines "Student" class
+"""
 
-students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
 
-for student in students:
-    j_student = student.to_json()
-    print(type(j_student))
-    print(j_student['first_name'])
-    print(type(j_student['first_name']))
-    print(j_student['age'])
-    print(type(j_student['age']))
+class Student:
+    """Student """
+    def __init__(self, first_name, last_name, age):
+        """Initializes student's information"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student instance """
+        return self.__dict__

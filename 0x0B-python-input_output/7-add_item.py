56fc51799db7a7bac4a0b19014ca2d48b3781880
Iam_fortunatus <amfortunatus@gmail.com>
2023-01-11 00:14:27 +0100

    0x0B-python-input_output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
+"""Module for saving to json"""
+import json
+import os.path
+import sys
 from sys import argv
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-list1 = []
+filename = "add_item.json"
+json_list = []
 
-try:
-    list1 = list(load_from_json_file("add_item.json"))
+if os.path.exists(filename):
+    json_list = load_from_json_file(filename)
 
-except:
-    len(list1) == 0
+for index in argv[1:]:
+    json_list.append(index)
 
-for arg in range(1, len(argv)):
-    list1.append(argv[arg])
-
-save_to_json_file(list1, "add_item.json")
+save_to_json_file(json_list, filename)

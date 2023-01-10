c723c3a42e463d89f666a8f3f164f452ccfe212c
Iam_fortunatus <amfortunatus@gmail.com>
2023-01-10 23:59:14 +0100

    0x0B-python-input_output

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+""" pascal triangle"""
 
-    def pascal_triangle(n):
-        list1 = []
-        for i in range(1, n + 1):
-            for j in range(1, i + 1):
-                list1.append(j)
-        return list1
+
+def pascal_triangle(n=4500):
+    """print pascal"""
+    pascal = [[0]*i for i in range(1, n+1)]
+    cmpt = 0
+    for i in range(n):
+        pascal[i][0] = 1
+        pascal[i][-1] = 1
+        for j in range(0, i//2):
+            pascal[i][j+1] = pascal[i-1][j] + pascal[i-1][j+1]
+            pascal[i][i-j-1] = pascal[i-1][j] + pascal[i-1][j+1]
+
+    return pascal

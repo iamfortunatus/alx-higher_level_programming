5c2cc44d29a781d37b9162eab010aef4fbabf302
Iam_fortunatus <amfortunatus@gmail.com>
2023-03-30 23:30:00 +0100

    0x10-python-network_0

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that sends a POST request and displays the body response
-curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
+# takes in a URL, sends a POST request to the passed URL
+curl -s $1 -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
